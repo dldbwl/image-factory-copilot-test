03733a87326fbab1fc3aacc0c10f0930cf7431c5
dldbwl <83522967+dldbwl@users.noreply.github.com>
2026-05-27 21:27:54 +0900

    feat: Add Gnb component

---
+++ src/components/Gnb.tsx
@@ -0,0 +1,13 @@
+export default function Gnb() {
+  return (
+    <div className="bg-dark-surface h-17 relative shrink-0 w-full border-b border-dark-border">
+      <div className="flex flex-row items-center h-full">
+        <div className="flex items-center px-7.5 relative h-full">
+          <div className="flex items-center w-29">
+            <h1 className="text-xl font-bold text-text-primary">Image Factory</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
